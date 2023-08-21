7fd419d456c352ee0cc9609390098dec17ac7238
myoji2 <hyeojin1719@gmail.com>
2023-08-21 12:48:47 +0900

    category section responsive

--- js/custom.js
+++ js/custom.js
@@ -49,3 +49,40 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+// mobile header toggle active
+
+// 1. 아이콘 클릭 -> 2. 아이콘에 'on' 클래스 추가 -> 3. 네비게이션 높이 저장 -> 4. 'on' 클래스 있을 때 네비게이션 활성화 -> 5. 'on' 클래스 없을 때 네비게이션 비 활성화
+
+const menuIcon = document.querySelector('.menu-icon'); // 메뉴 아이콘 요소 저장
+const navi = document.querySelector('.navi'); //네비게이션 박스 요소 저장
+
+//console.log('menuIcon', menuIcon); console은 확인용
+//console.log('navi', navi);
+
+menuIcon.addEventListener('click', function () {
+  this.classList.toggle('on'); //menu-icon 클릭 할 때마다 on 클래스 추가 및 제거
+
+  const navHeight = navi.scrollHeight;
+  console.log(navHeight);
+
+  if (this.classList.contains('on')) {
+    // console.log(true); // menu-icon에 on 클래스가 있다면
+    navi.style.height = navHeight + 'px';
+  } else {
+    console.log(false); // menu-icon에 on 클래스가 없다면
+    navi.style.height = 0;
+  }
+});
+
+// If PC size browser, navigation height to normal
+window.addEventListener('resize', function () {
+  const winWidth = window.outerWidth; //윈도우 화면 가로 사이즈 저장
+
+  if (winWidth > 980) {
+    menuIcon.classList.remove('on');
+    navi.style.height = 'auto';
+  } else {
+    navi.style.height = 0;
+  }
+});
